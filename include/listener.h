f438bd836a5c8f4c84e1a74d9dce2aae200bd526
Valery Yatsko <darkwire.tl@gmail.com>
2008-04-02 13:11:59 +0400

    Reverting last commit to try again

--- include/listener.h
+++ include/listener.h
@@ -33,21 +33,22 @@ struct Client;
 
 struct Listener
 {
-	rb_dlink_node node;
+	struct Listener *next;	/* list node pointer */
 	const char *name;	/* listener name */
-	rb_fde_t *F;		/* file descriptor */
+	int fd;			/* file descriptor */
 	int ref_count;		/* number of connection references */
 	int active;		/* current state of listener */
-	int ssl;		/* ssl listener */
-	struct rb_sockaddr_storage addr;
+	int index;		/* index into poll array */
+	struct irc_sockaddr_storage addr;
+	struct DNSQuery *dns_query;
 	char vhost[HOSTLEN + 1];	/* virtual name of listener */
 };
 
-void add_listener(int port, const char *vaddr_ip, int family, int ssl);
-void close_listener(struct Listener *listener);
-void close_listeners(void);
-const char *get_listener_name(struct Listener *listener);
-void show_ports(struct Client *client);
-void free_listener(struct Listener *);
+extern void add_listener(int port, const char *vaddr_ip, int family);
+extern void close_listener(struct Listener *listener);
+extern void close_listeners(void);
+extern const char *get_listener_name(const struct Listener *listener);
+extern void show_ports(struct Client *client);
+extern void free_listener(struct Listener *);
 
 #endif /* INCLUDED_listener_h */
--- src/newconf.c
+++ src/newconf.c
@@ -758,9 +758,9 @@ conf_set_listen_port(void *data)
 		}
                 if(listener_address == NULL)
                 {
-			add_listener(args->v.number, listener_address, AF_INET, 0);
+			add_listener(args->v.number, listener_address, AF_INET);
 #ifdef IPV6
-			add_listener(args->v.number, listener_address, AF_INET6, 0);
+			add_listener(args->v.number, listener_address, AF_INET6);
 #endif
                 }
 		else
@@ -773,7 +773,7 @@ conf_set_listen_port(void *data)
 #endif
 				family = AF_INET;
 		
-			add_listener(args->v.number, listener_address, family, 0);
+			add_listener(args->v.number, listener_address, family);
                 
                 }
 
