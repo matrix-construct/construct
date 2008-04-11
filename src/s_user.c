eae062741dfcd64f12e6d4ed9d78bf8fb0fded4f
Valery Yatsko <darkwire@darkwire.ru>
2008-04-11 19:09:19 +0400

    'You are exempt from...' message fixed - glines gone

--- src/s_user.c
+++ src/s_user.c
@@ -815,7 +815,7 @@ report_and_set_user_flags(struct Client *source_p, struct ConfItem *aconf)
 	if(IsConfExemptKline(aconf))
 	{
 		SetExemptKline(source_p);
-		sendto_one_notice(source_p, ":*** You are exempt from K/G/X lines. congrats.");
+		sendto_one_notice(source_p, ":*** You are exempt from K/X lines. congrats.");
 	}
 
 	if(IsConfExemptDNSBL(aconf))
