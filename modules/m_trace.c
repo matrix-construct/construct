a672fbb78c7e07a6f9f45e6775a9198b3ef93c66
Jilles Tjoelker <jilles@stack.nl>
2008-06-28 21:08:04 +0200

    Make m_trace.c compile here (with gcc 4.2.1).

--- modules/m_trace.c
+++ modules/m_trace.c
@@ -380,18 +380,18 @@ report_this_status(struct Client *source_p, struct Client *target_p)
 		break;
 
 	case STAT_CLIENT:
-		int tnumeric = RPL_TRACEUSER;
-		
-		if(IsOper(target_p))
-			tnumeric = RPL_TRACEOPERATOR;
-			
-		sendto_one_numeric(source_p, tnumeric, form_str(tnumeric),
-				class_name, name,
-				show_ip(source_p, target_p) ? ip : empty_sockhost,
-				rb_current_time() - target_p->localClient->lasttime,
-				rb_current_time() - target_p->localClient->last);
+		{
+			int tnumeric;
 
-		cnt++;
+			tnumeric = IsOper(target_p) ? RPL_TRACEOPERATOR : RPL_TRACEUSER;
+			sendto_one_numeric(source_p, tnumeric, form_str(tnumeric),
+					class_name, name,
+					show_ip(source_p, target_p) ? ip : empty_sockhost,
+					rb_current_time() - target_p->localClient->lasttime,
+					rb_current_time() - target_p->localClient->last);
+
+			cnt++;
+		}
 		break;
 
 	case STAT_SERVER:
