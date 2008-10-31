9641f15652b34411e1b5306d6067ea87778982a1
Jilles Tjoelker <jilles@stack.nl>
2008-10-31 10:57:51 +0100

    Add the IP address to userlog, as in ratbox3.

--- src/client.c
+++ src/client.c
@@ -1484,10 +1484,11 @@ exit_local_client(struct Client *client_p, struct Client *source_p, struct Clien
 
 	on_for = rb_current_time() - source_p->localClient->firsttime;
 
-	ilog(L_USER, "%s (%3lu:%02lu:%02lu): %s!%s@%s %d/%d",
+	ilog(L_USER, "%s (%3lu:%02lu:%02lu): %s!%s@%s %s %d/%d",
 		rb_ctime(rb_current_time(), tbuf, sizeof(tbuf)), on_for / 3600,
 		(on_for % 3600) / 60, on_for % 60,
 		source_p->name, source_p->username, source_p->host,
+		source_p->sockhost,
 		source_p->localClient->sendK, source_p->localClient->receiveK);
 
 	sendto_one(source_p, "ERROR :Closing Link: %s (%s)", source_p->host, comment);
