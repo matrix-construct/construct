bde6442c472e577aa55de10317857bed2dd0de12
William Pitcock <nenolod@atheme.org>
2010-01-19 01:47:21 -0600

    supported: Add CLIENTVER=3.0 to indicate presence of 3.0 core capabilities (such as CAP, extended WHO, etc) as requested by LifeIsPain.

--- src/supported.c
+++ src/supported.c
@@ -325,4 +325,5 @@ init_isupport(void)
 	add_isupport("TARGMAX", isupport_targmax, NULL);
 	add_isupport("EXTBAN", isupport_extban, NULL);
 	add_isupport("WHOX", isupport_string, "");
+	add_isupport("CLIENTVER", isupport_string, "3.0");
 }
