f1e5a3177524f838b4c4da8b8f1c93b7e0a6fe70
JD Horelick <jdhore1@gmail.com>
2010-11-13 16:07:54 -0500

    Change the filenames of the generated SSL key/cert to ssl.* instead of test.* so that they're a bit more descriptive.

--- tools/genssl.sh
+++ tools/genssl.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 echo "Generating self-signed certificate .. "
-openssl req -x509 -nodes -newkey rsa:1024 -keyout ../etc/test.key -out ../etc/test.cert
+openssl req -x509 -nodes -newkey rsa:1024 -keyout ../etc/ssl.key -out ../etc/ssl.cert
 
 echo "Generating Diffie-Hellman file for secure SSL/TLS negotiation .. "
 openssl dhparam -out ../etc/dh.pem 1024
@@ -8,8 +8,8 @@ openssl dhparam -out ../etc/dh.pem 1024
 echo " 
 Now change these lines in the IRCd config file:
 
-    ssl_private_key = "etc/test.key";
-    ssl_cert = "etc/test.cert";
+    ssl_private_key = "etc/ssl.key";
+    ssl_cert = "etc/ssl.cert";
     ssl_dh_params = "etc/dh.pem";
 
 Enjoy using ssl.
