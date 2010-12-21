0c512421c50e8855539e3f5c0e13a6b1e9ba06ce
Stephen Bennett <spb@exherbo.org>
2010-12-21 20:53:39 +0000

    Add default for disable_local_channels. Missed this last time.

--- src/s_conf.c
+++ src/s_conf.c
@@ -773,6 +773,7 @@ set_default_conf(void)
 	ConfigChannel.no_create_on_split = YES;
 	ConfigChannel.resv_forcepart = YES;
 	ConfigChannel.channel_target_change = YES;
+	ConfigChannel.disable_local_channels = NO;
 
 	ConfigServerHide.flatten_links = 0;
 	ConfigServerHide.links_delay = 300;
