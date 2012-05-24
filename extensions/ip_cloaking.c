matrix-construct/construct

commit 29d224a1f804c0c159bf52e6a939f93ff07b5bbb

ip_cloaking*: Do qjm locally too.

extensions/ip_cloaking.c
@@ -47,25 +47,28 @@ DECLARE_MODULE_AV1(ip_cloaking, _modinit, _moddeinit, NULL, NULL,
 			ip_cloaking_hfnlist, "$Revision: 3526 $");
 
 static void
-distribute_hostchange(struct Client *client)
+distribute_hostchange(struct Client *client_p, char *newhost)
 {
-	if (irccmp(client->host, client->orighost))
-		sendto_one_numeric(client, RPL_HOSTHIDDEN, "%s :is now your hidden host",
-			client->host);
+	if (newhost != client_p->orighost)
+		sendto_one_numeric(client_p, RPL_HOSTHIDDEN, "%s :is now your hidden host",
+			client_p->host);
 	else
-		sendto_one_numeric(client, RPL_HOSTHIDDEN, "%s :hostname reset",
-			client->host);
+		sendto_one_numeric(client_p, RPL_HOSTHIDDEN, "%s :hostname reset",
+			client_p->host);
 
 	sendto_server(NULL, NULL,
 		CAP_EUID | CAP_TS6, NOCAPS, ":%s CHGHOST %s :%s",
-		use_id(&me), use_id(client), client->host);
+		use_id(&me), use_id(client_p), client_p->host);
 	sendto_server(NULL, NULL,
 		CAP_TS6, CAP_EUID, ":%s ENCAP * CHGHOST %s :%s",
-		use_id(&me), use_id(client), client->host);
-	if (irccmp(client->host, client->orighost))
-		SetDynSpoof(client);
+		use_id(&me), use_id(client_p), client_p->host);
+
+	change_nick_user_host(client_p, client_p->name, client_p->username, newhost, 0, "Changing host");
+
+	if (newhost != client_p->orighost)
+		SetDynSpoof(client_p);
 	else
-		ClearDynSpoof(client);
+		ClearDynSpoof(client_p);
 }
 
 static void
@@ -179,8 +182,7 @@ check_umode_change(void *vdata)
 		}
 		if (strcmp(source_p->host, source_p->localClient->mangledhost))
 		{
-			rb_strlcpy(source_p->host, source_p->localClient->mangledhost, HOSTLEN + 1);
-			distribute_hostchange(source_p);
+			distribute_hostchange(source_p, source_p->localClient->mangledhost);
 		}
 		else /* not really nice, but we need to send this numeric here */
 			sendto_one_numeric(source_p, RPL_HOSTHIDDEN, "%s :is now your hidden host",
@@ -191,8 +193,7 @@ check_umode_change(void *vdata)
 		if (source_p->localClient->mangledhost != NULL &&
 				!strcmp(source_p->host, source_p->localClient->mangledhost))
 		{
-			rb_strlcpy(source_p->host, source_p->orighost, HOSTLEN + 1);
-			distribute_hostchange(source_p);
+			distribute_hostchange(source_p, source_p->orighost);
 		}
 	}
 }

extensions/ip_cloaking_3.0.c
@@ -44,25 +44,28 @@ DECLARE_MODULE_AV1(ip_cloaking, _modinit, _moddeinit, NULL, NULL,
 			ip_cloaking_hfnlist, "$Revision: 3526 $");
 
 static void
-distribute_hostchange(struct Client *client)
+distribute_hostchange(struct Client *client_p, char *newhost)
 {
-	if (irccmp(client->host, client->orighost))
-		sendto_one_numeric(client, RPL_HOSTHIDDEN, "%s :is now your hidden host",
-			client->host);
+	if (newhost != client_p->orighost)
+		sendto_one_numeric(client_p, RPL_HOSTHIDDEN, "%s :is now your hidden host",
+			client_p->host);
 	else
-		sendto_one_numeric(client, RPL_HOSTHIDDEN, "%s :hostname reset",
-			client->host);
+		sendto_one_numeric(client_p, RPL_HOSTHIDDEN, "%s :hostname reset",
+			client_p->host);
 
 	sendto_server(NULL, NULL,
 		CAP_EUID | CAP_TS6, NOCAPS, ":%s CHGHOST %s :%s",
-		use_id(&me), use_id(client), client->host);
+		use_id(&me), use_id(client_p), client_p->host);
 	sendto_server(NULL, NULL,
 		CAP_TS6, CAP_EUID, ":%s ENCAP * CHGHOST %s :%s",
-		use_id(&me), use_id(client), client->host);
-	if (irccmp(client->host, client->orighost))
-		SetDynSpoof(client);
+		use_id(&me), use_id(client_p), client_p->host);
+
+	change_nick_user_host(client_p, client_p->name, client_p->username, newhost, 0, "Changing host");
+
+	if (newhost != client_p->orighost)
+		SetDynSpoof(client_p);
 	else
-		ClearDynSpoof(client);
+		ClearDynSpoof(client_p);
 }
 
 #define Nval 0x8c3a48ac
@@ -186,8 +189,7 @@ check_umode_change(void *vdata)
 		}
 		if (strcmp(source_p->host, source_p->localClient->mangledhost))
 		{
-			rb_strlcpy(source_p->host, source_p->localClient->mangledhost, HOSTLEN);
-			distribute_hostchange(source_p);
+			distribute_hostchange(source_p, source_p->localClient->mangledhost);
 		}
 		else /* not really nice, but we need to send this numeric here */
 			sendto_one_numeric(source_p, RPL_HOSTHIDDEN, "%s :is now your hidden host",
@@ -198,8 +200,7 @@ check_umode_change(void *vdata)
 		if (source_p->localClient->mangledhost != NULL &&
 				!strcmp(source_p->host, source_p->localClient->mangledhost))
 		{
-			rb_strlcpy(source_p->host, source_p->orighost, HOSTLEN);
-			distribute_hostchange(source_p);
+			distribute_hostchange(source_p, source_p->orighost);
 		}
 	}
 }

extensions/ip_cloaking_4.0.c
@@ -47,25 +47,28 @@ DECLARE_MODULE_AV1(ip_cloaking, _modinit, _moddeinit, NULL, NULL,
 			ip_cloaking_hfnlist, "$Revision: 3526 $");
 
 static void
-distribute_hostchange(struct Client *client)
+distribute_hostchange(struct Client *client_p, char *newhost)
 {
-	if (irccmp(client->host, client->orighost))
-		sendto_one_numeric(client, RPL_HOSTHIDDEN, "%s :is now your hidden host",
-			client->host);
+	if (newhost != client_p->orighost)
+		sendto_one_numeric(client_p, RPL_HOSTHIDDEN, "%s :is now your hidden host",
+			client_p->host);
 	else
-		sendto_one_numeric(client, RPL_HOSTHIDDEN, "%s :hostname reset",
-			client->host);
+		sendto_one_numeric(client_p, RPL_HOSTHIDDEN, "%s :hostname reset",
+			client_p->host);
 
 	sendto_server(NULL, NULL,
 		CAP_EUID | CAP_TS6, NOCAPS, ":%s CHGHOST %s :%s",
-		use_id(&me), use_id(client), client->host);
+		use_id(&me), use_id(client_p), client_p->host);
 	sendto_server(NULL, NULL,
 		CAP_TS6, CAP_EUID, ":%s ENCAP * CHGHOST %s :%s",
-		use_id(&me), use_id(client), client->host);
-	if (irccmp(client->host, client->orighost))
-		SetDynSpoof(client);
+		use_id(&me), use_id(client_p), client_p->host);
+
+	change_nick_user_host(client_p, client_p->name, client_p->username, newhost, 0, "Changing host");
+
+	if (newhost != client_p->orighost)
+		SetDynSpoof(client_p);
 	else
-		ClearDynSpoof(client);
+		ClearDynSpoof(client_p);
 }
 
 static void
@@ -179,8 +182,7 @@ check_umode_change(void *vdata)
 		}
 		if (strcmp(source_p->host, source_p->localClient->mangledhost))
 		{
-			rb_strlcpy(source_p->host, source_p->localClient->mangledhost, HOSTLEN + 1);
-			distribute_hostchange(source_p);
+			distribute_hostchange(source_p, source_p->localClient->mangledhost);
 		}
 		else /* not really nice, but we need to send this numeric here */
 			sendto_one_numeric(source_p, RPL_HOSTHIDDEN, "%s :is now your hidden host",
@@ -191,8 +193,7 @@ check_umode_change(void *vdata)
 		if (source_p->localClient->mangledhost != NULL &&
 				!strcmp(source_p->host, source_p->localClient->mangledhost))
 		{
-			rb_strlcpy(source_p->host, source_p->orighost, HOSTLEN + 1);
-			distribute_hostchange(source_p);
+			distribute_hostchange(source_p, source_p->orighost);
 		}
 	}
 }

extensions/ip_cloaking_old.c
@@ -44,25 +44,28 @@ DECLARE_MODULE_AV1(ip_cloaking, _modinit, _moddeinit, NULL, NULL,
 			ip_cloaking_hfnlist, "$Revision: 3522 $");
 
 static void
-distribute_hostchange(struct Client *client)
+distribute_hostchange(struct Client *client_p, char *newhost)
 {
-	if (irccmp(client->host, client->orighost))
-		sendto_one_numeric(client, RPL_HOSTHIDDEN, "%s :is now your hidden host",
-			client->host);
+	if (newhost != client_p->orighost)
+		sendto_one_numeric(client_p, RPL_HOSTHIDDEN, "%s :is now your hidden host",
+			client_p->host);
 	else
-		sendto_one_numeric(client, RPL_HOSTHIDDEN, "%s :hostname reset",
-			client->host);
+		sendto_one_numeric(client_p, RPL_HOSTHIDDEN, "%s :hostname reset",
+			client_p->host);
 
 	sendto_server(NULL, NULL,
 		CAP_EUID | CAP_TS6, NOCAPS, ":%s CHGHOST %s :%s",
-		use_id(&me), use_id(client), client->host);
+		use_id(&me), use_id(client_p), client_p->host);
 	sendto_server(NULL, NULL,
 		CAP_TS6, CAP_EUID, ":%s ENCAP * CHGHOST %s :%s",
-		use_id(&me), use_id(client), client->host);
-	if (irccmp(client->host, client->orighost))
-		SetDynSpoof(client);
+		use_id(&me), use_id(client_p), client_p->host);
+
+	change_nick_user_host(client_p, client_p->name, client_p->username, newhost, 0, "Changing host");
+
+	if (newhost != client_p->orighost)
+		SetDynSpoof(client_p);
 	else
-		ClearDynSpoof(client);
+		ClearDynSpoof(client_p);
 }
 
 static void
@@ -128,8 +131,7 @@ check_umode_change(void *vdata)
 		}
 		if (strcmp(source_p->host, source_p->localClient->mangledhost))
 		{
-			rb_strlcpy(source_p->host, source_p->localClient->mangledhost, HOSTLEN);
-			distribute_hostchange(source_p);
+			distribute_hostchange(source_p, source_p->localClient->mangledhost);
 		}
 		else /* not really nice, but we need to send this numeric here */
 			sendto_one_numeric(source_p, RPL_HOSTHIDDEN, "%s :is now your hidden host",
@@ -140,8 +142,7 @@ check_umode_change(void *vdata)
 		if (source_p->localClient->mangledhost != NULL &&
 				!strcmp(source_p->host, source_p->localClient->mangledhost))
 		{
-			rb_strlcpy(source_p->host, source_p->orighost, HOSTLEN);
-			distribute_hostchange(source_p);
+			distribute_hostchange(source_p, source_p->orighost);
 		}
 	}
 }

commit 9f409b6333a6da3ea7a1756d5108eb80ad204310

extensions/ip_cloaking*: Fix a bug accidentally introduced in 29d224a1 where the cloaking module would change your cloak locally, but not correctly broadcast this to the network, and fail to tell you correctly in the RPL_HOSTHIDDEN reply

extensions/ip_cloaking.c
@@ -51,17 +51,17 @@ distribute_hostchange(struct Client *client_p, char *newhost)
 {
 	if (newhost != client_p->orighost)
 		sendto_one_numeric(client_p, RPL_HOSTHIDDEN, "%s :is now your hidden host",
-			client_p->host);
+			newhost);
 	else
 		sendto_one_numeric(client_p, RPL_HOSTHIDDEN, "%s :hostname reset",
-			client_p->host);
+			newhost);
 
 	sendto_server(NULL, NULL,
 		CAP_EUID | CAP_TS6, NOCAPS, ":%s CHGHOST %s :%s",
-		use_id(&me), use_id(client_p), client_p->host);
+		use_id(&me), use_id(client_p), newhost);
 	sendto_server(NULL, NULL,
 		CAP_TS6, CAP_EUID, ":%s ENCAP * CHGHOST %s :%s",
-		use_id(&me), use_id(client_p), client_p->host);
+		use_id(&me), use_id(client_p), newhost);
 
 	change_nick_user_host(client_p, client_p->name, client_p->username, newhost, 0, "Changing host");
 

extensions/ip_cloaking_3.0.c
@@ -48,17 +48,17 @@ distribute_hostchange(struct Client *client_p, char *newhost)
 {
 	if (newhost != client_p->orighost)
 		sendto_one_numeric(client_p, RPL_HOSTHIDDEN, "%s :is now your hidden host",
-			client_p->host);
+			newhost);
 	else
 		sendto_one_numeric(client_p, RPL_HOSTHIDDEN, "%s :hostname reset",
-			client_p->host);
+			newhost);
 
 	sendto_server(NULL, NULL,
 		CAP_EUID | CAP_TS6, NOCAPS, ":%s CHGHOST %s :%s",
-		use_id(&me), use_id(client_p), client_p->host);
+		use_id(&me), use_id(client_p), newhost);
 	sendto_server(NULL, NULL,
 		CAP_TS6, CAP_EUID, ":%s ENCAP * CHGHOST %s :%s",
-		use_id(&me), use_id(client_p), client_p->host);
+		use_id(&me), use_id(client_p), newhost);
 
 	change_nick_user_host(client_p, client_p->name, client_p->username, newhost, 0, "Changing host");
 

extensions/ip_cloaking_4.0.c
@@ -51,17 +51,17 @@ distribute_hostchange(struct Client *client_p, char *newhost)
 {
 	if (newhost != client_p->orighost)
 		sendto_one_numeric(client_p, RPL_HOSTHIDDEN, "%s :is now your hidden host",
-			client_p->host);
+			newhost);
 	else
 		sendto_one_numeric(client_p, RPL_HOSTHIDDEN, "%s :hostname reset",
-			client_p->host);
+			newhost);
 
 	sendto_server(NULL, NULL,
 		CAP_EUID | CAP_TS6, NOCAPS, ":%s CHGHOST %s :%s",
-		use_id(&me), use_id(client_p), client_p->host);
+		use_id(&me), use_id(client_p), newhost);
 	sendto_server(NULL, NULL,
 		CAP_TS6, CAP_EUID, ":%s ENCAP * CHGHOST %s :%s",
-		use_id(&me), use_id(client_p), client_p->host);
+		use_id(&me), use_id(client_p), newhost);
 
 	change_nick_user_host(client_p, client_p->name, client_p->username, newhost, 0, "Changing host");
 

extensions/ip_cloaking_old.c
@@ -48,17 +48,17 @@ distribute_hostchange(struct Client *client_p, char *newhost)
 {
 	if (newhost != client_p->orighost)
 		sendto_one_numeric(client_p, RPL_HOSTHIDDEN, "%s :is now your hidden host",
-			client_p->host);
+			newhost);
 	else
 		sendto_one_numeric(client_p, RPL_HOSTHIDDEN, "%s :hostname reset",
-			client_p->host);
+			newhost);
 
 	sendto_server(NULL, NULL,
 		CAP_EUID | CAP_TS6, NOCAPS, ":%s CHGHOST %s :%s",
-		use_id(&me), use_id(client_p), client_p->host);
+		use_id(&me), use_id(client_p), newhost);
 	sendto_server(NULL, NULL,
 		CAP_TS6, CAP_EUID, ":%s ENCAP * CHGHOST %s :%s",
-		use_id(&me), use_id(client_p), client_p->host);
+		use_id(&me), use_id(client_p), newhost);
 
 	change_nick_user_host(client_p, client_p->name, client_p->username, newhost, 0, "Changing host");
 