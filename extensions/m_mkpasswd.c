f9960c026200a7abda9575cb80fb397dbf705d7e
Jilles Tjoelker <jilles@stack.nl>
2014-02-23 22:18:44 +0100

    extensions/mkpasswd: Do not leak /dev/random fd.

--- extensions/m_mkpasswd.c
+++ extensions/m_mkpasswd.c
@@ -210,6 +210,7 @@ generate_random_salt(char *salt, int length)
 	if(read(fd, buf, length) != length)
 	{
 		free(buf);
+		close(fd);
 		return (generate_poor_salt(salt, length));
 	}
 
@@ -218,5 +219,6 @@ generate_random_salt(char *salt, int length)
 		salt[i] = saltChars[abs(buf[i]) % 64];
 	}
 	free(buf);
+	close(fd);
 	return (salt);
 }
