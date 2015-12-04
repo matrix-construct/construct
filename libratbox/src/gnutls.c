fab6f9e80ef3f0faebfec7c1129e2c1e0f12b8ed
William Pitcock <nenolod@dereferenced.org>
2015-12-03 22:38:54 -0600

    libratbox: gnutls: we want to pass F, not F->fd to rb_listen()

--- libratbox/src/gnutls.c
+++ libratbox/src/gnutls.c
@@ -410,7 +410,7 @@ rb_ssl_listen(rb_fde_t *F, int backlog, int defer_accept)
 {
 	int result;
 
-	result = rb_listen(F->fd, backlog, defer_accept);
+	result = rb_listen(F, backlog, defer_accept);
 	F->type = RB_FD_SOCKET | RB_FD_LISTEN | RB_FD_SSL;
 
 	return result;
