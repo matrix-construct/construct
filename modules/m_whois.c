e00552d5cec2d48c08c15b704e2ffa80b9556037
William Pitcock <nenolod@dereferenced.org>
2015-12-28 01:38:13 -0600

    whois: cosmetic improvement to opered-as numeric

--- modules/m_whois.c
+++ modules/m_whois.c
@@ -323,7 +323,7 @@ single_whois(struct Client *source_p, struct Client *target_p, int operspy)
 	   (IsOperAdmin(source_p) || IsAdmin(source_p) || (source_p == target_p)))
 	{
 		char buf[512];
-		rb_snprintf(buf, sizeof(buf), "Opered as %s, privset %s",
+		rb_snprintf(buf, sizeof(buf), "is opered as %s, privset %s",
 			    target_p->localClient->opername, target_p->localClient->privset->name);
 		sendto_one_numeric(source_p, RPL_WHOISSPECIAL, form_str(RPL_WHOISSPECIAL),
 				   target_p->name, buf);
