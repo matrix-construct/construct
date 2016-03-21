6e2ed9da91cbdb2e91c356e6bb35916066297d9f
William Pitcock <nenolod@dereferenced.org>
2016-03-20 21:34:03 -0500

    appveyor: simplify build script further

--- .appveyor-build.sh
+++ .appveyor-build.sh
@@ -2,8 +2,6 @@ set -v
 
 export PATH=/mingw64/bin:/usr/local/bin:/usr/bin:/bin:/c/Windows/system32:/c/Windows:/c/Windows/System32/Wbem:/c/Windows/System32/WindowsPowerShell/v1.0:/usr/bin/site_perl:/usr/bin/vendor_perl:/usr/bin/core_perl
 
-cd $1
-
 sh ./autogen.sh
 ./configure --prefix=c:/projects/charybdis/build --enable-openssl=/mingw64
 make -j2
