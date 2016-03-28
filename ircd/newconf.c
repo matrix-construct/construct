771dcfad37625d650fc0dfc3e898e06250ad5490
Elizabeth Myers <elizabeth@interlinked.me>
2016-03-28 17:54:01 -0500

    newconf: add illustrative examples to comments

--- ircd/newconf.c
+++ ircd/newconf.c
@@ -1971,7 +1971,9 @@ conf_set_blacklist_reason(void *data)
 		yy_blacklist_reason = rb_strdup(data);
 		if (yy_blacklist_iptype & IPTYPE_IPV6)
 		{
-			/* Make sure things fit (64 = alnum count + dots) */
+			/* Make sure things fit (64 = alnum count + dots in rDNS notation)
+			 * Example: 1.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.ip6.arpa
+			 */
 			if ((64 + strlen(yy_blacklist_host)) > IRCD_RES_HOSTLEN)
 			{
 				conf_report_error("blacklist::host %s results in IPv6 queries that are too long",
@@ -1982,7 +1984,9 @@ conf_set_blacklist_reason(void *data)
 		/* Avoid doing redundant check, IPv6 is bigger than IPv4 --Elizabeth */
 		if ((yy_blacklist_iptype & IPTYPE_IPV4) && !(yy_blacklist_iptype & IPTYPE_IPV6))
 		{
-			/* Make sure things fit (16 = number of nums + dots) */
+			/* Make sure things fit (16 = number of nums + dots)
+			 * Example: 1.0.0.127.in-addr.arpa
+			 */
 			if ((16 + strlen(yy_blacklist_host)) > IRCD_RES_HOSTLEN)
 			{
 				conf_report_error("blacklist::host %s results in IPv4 queries that are too long",
