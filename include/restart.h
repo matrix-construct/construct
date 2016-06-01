aa7b99eb77b2ec91cb611c8ee132348e50b948da
Aaron Jones <aaronmdjones@gmail.com>
2016-06-01 20:14:09 +0000

    restart: functions that call exit(3) should be marked noreturn

--- include/restart.h
+++ include/restart.h
@@ -25,7 +25,7 @@
 #ifndef INCLUDED_restart_h
 #define INCLUDED_restart_h
 
-void restart(const char *);
-void server_reboot(void);
+void restart(const char *) __attribute__((noreturn));
+void server_reboot(void) __attribute__((noreturn));
 
 #endif
