c5698b02769f1efbf163d0fcc9e150fcf42bac82
Jason Volk <jason@zemos.net>
2016-06-27 15:21:23 -0700

    librb: Move bool type stuff from ircd stdinc to rb.

--- librb/include/rb_lib.h
+++ librb/include/rb_lib.h
@@ -10,6 +10,24 @@
 #include <signal.h>
 #include <ctype.h>
 
+
+#ifdef HAVE_STDBOOL_H
+	#include <stdbool.h>
+#else
+	#ifndef HAVE__BOOL
+		#ifdef __cplusplus
+			typedef bool _Bool;
+		#else
+			#define _Bool signed char
+		#endif
+	#endif
+	#define bool _Bool
+	#define false 0
+	#define true 1
+	#define __bool_true_false_are_defined 1
+#endif
+
+
 #ifdef __GNUC__
 #undef alloca
 #define alloca __builtin_alloca
