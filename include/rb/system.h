13a18ace2b8360633f41c96bd98da38e68376d9d
Jason Volk <jason@zemos.net>
2016-08-17 19:48:44 -0700

    rb: Remove defines/includes related to booltype for __cplusplus.

--- include/rb/system.h
+++ include/rb/system.h
@@ -8,21 +8,18 @@ extern "C" {
 	#define RB_IPV6 1
 #endif
 
-
-#ifdef HAVE_STDBOOL_H
-	#include <stdbool.h>
-#else
-	#ifndef HAVE__BOOL
-		#ifdef __cplusplus
-			typedef bool _Bool;
-		#else
+#ifndef __cplusplus
+	#ifdef HAVE_STDBOOL_H
+		#include <stdbool.h>
+	#else
+		#ifndef HAVE__BOOL
 			#define _Bool signed char
 		#endif
+		#define bool _Bool
+		#define false 0
+		#define true 1
+		#define __bool_true_false_are_defined 1
 	#endif
-	#define bool _Bool
-	#define false 0
-	#define true 1
-	#define __bool_true_false_are_defined 1
 #endif
 
 
