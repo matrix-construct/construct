a65a033c3b030657ba5023ff8104273b4b6bd184
Jason Volk <jason@zemos.net>
2017-09-23 20:37:44 -0700

    ircd::db: Add reverse long int comparator; fix reverse string comparator; cleanup.

--- include/ircd/db/comparator.h
+++ include/ircd/db/comparator.h
@@ -28,6 +28,8 @@ namespace ircd::db
 	struct comparator;
 
 	struct cmp_int64_t;
+	struct reverse_cmp_int64_t;
+
 	struct cmp_string_view;
 	struct reverse_cmp_string_view;
 }
@@ -37,24 +39,6 @@ struct ircd::db::comparator
 	string_view name;
 	std::function<bool (const string_view &, const string_view &)> less;
 	std::function<bool (const string_view &, const string_view &)> equal;
-};
-
-struct ircd::db::reverse_cmp_string_view
-:db::comparator
-{
-	static bool less(const string_view &a, const string_view &b)
-	{
-		return std::memcmp(a.data(), b.data(), std::min(a.size(), b.size())) > 0;
-	}
-
-	static bool equal(const string_view &a, const string_view &b)
-	{
-		return a == b;
-	}
-
-	reverse_cmp_string_view()
-	:db::comparator{"reverse_string_view", &less, &equal}
-	{}
 };
 
 struct ircd::db::cmp_string_view
@@ -75,28 +59,69 @@ struct ircd::db::cmp_string_view
 	{}
 };
 
+struct ircd::db::reverse_cmp_string_view
+:db::comparator
+{
+	static bool less(const string_view &a, const string_view &b)
+	{
+		/// RocksDB sez things will not work correctly unless a shorter string
+		/// result returns less than a longer string even if one intends some
+		/// reverse ordering
+		if(a.size() < b.size())
+			return true;
+
+		/// Furthermore, b.size() < a.size() returning false from this function
+		/// appears to not be correct. The reversal also has to also come in
+		/// the form of a bytewise forward iteration.
+		return std::memcmp(a.data(), b.data(), std::min(a.size(), b.size())) > 0;
+	}
+
+	static bool equal(const string_view &a, const string_view &b)
+	{
+		return a == b;
+	}
+
+	reverse_cmp_string_view()
+	:db::comparator{"reverse_string_view", &less, &equal}
+	{}
+};
+
 struct ircd::db::cmp_int64_t
 :db::comparator
 {
 	static bool less(const string_view &sa, const string_view &sb)
 	{
-		assert(sa.size() == sizeof(int64_t));
-		assert(sb.size() == sizeof(int64_t));
-		const byte_view<int64_t> a{sa};
-		const byte_view<int64_t> b{sb};
+		assert(sa.size() == sizeof(int64_t) && sb.size() == sizeof(int64_t));
+		const byte_view<int64_t> a{sa}, b{sb};
 		return a < b;
 	}
 
 	static bool equal(const string_view &sa, const string_view &sb)
 	{
-		assert(sa.size() == sizeof(int64_t));
-		assert(sb.size() == sizeof(int64_t));
-		const byte_view<int64_t> a{sa};
-		const byte_view<int64_t> b{sb};
+		assert(sa.size() == sizeof(int64_t) && sb.size() == sizeof(int64_t));
+		const byte_view<int64_t> a{sa}, b{sb};
 		return a == b;
 	}
 
 	cmp_int64_t()
 	:db::comparator{"int64_t", &less, &equal}
+	{}
+};
+
+struct ircd::db::reverse_cmp_int64_t
+:db::comparator
+{
+	static bool less(const string_view &a, const string_view &b)
+	{
+		return !cmp_int64_t::less(a, b);
+	}
+
+	static bool equal(const string_view &a, const string_view &b)
+	{
+		return cmp_int64_t::equal(a, b);
+	}
+
+	reverse_cmp_int64_t()
+	:db::comparator{"reverse_int64_t", &less, &equal}
 	{}
 };
