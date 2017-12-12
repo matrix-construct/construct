5f6f66d74ea50bcde7923bf360d54d119c16eb12
Jason Volk <jason@zemos.net>
2017-12-12 14:01:15 -0700

    Remove old testsuite and scripts.

--- testsuite/startall.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-testdir=`pwd`
-prefix=`sed -n -e 's/^#define IRCD_PREFIX "\(.*\)"/\1/p' "$testdir/../include/setup.h"`
-[ -d $prefix ] || { echo Unable to find installation prefix; exit 1; }
-
-$prefix/bin/charybdis -configfile $testdir/ircd.conf.1 -pidfile $testdir/ircd.pid.1
-$prefix/bin/charybdis -configfile $testdir/ircd.conf.2 -pidfile $testdir/ircd.pid.2
-$prefix/bin/charybdis -configfile $testdir/ircd.conf.3 -pidfile $testdir/ircd.pid.3
