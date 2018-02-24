bc7efd8bff7b4456b53727bab3e474b40e714233
Jason Volk <jason@zemos.net>
2018-02-23 22:13:17 -0800

    modules/client: Stub publicised_groups endpoint.

---
+++ modules/client/publicised_groups.cc
@@ -0,0 +1,63 @@
+// Matrix Construct
+//
+// Copyright (C) Matrix Construct Developers, Authors & Contributors
+// Copyright (C) 2016-2018 Jason Volk <jason@zemos.net>
+//
+// Permission to use, copy, modify, and/or distribute this software for any
+// purpose with or without fee is hereby granted, provided that the above
+// copyright notice and this permission notice is present in all copies. The
+// full license for this software is available in the LICENSE file.
+
+using namespace ircd;
+
+mapi::header
+IRCD_MODULE
+{
+	"Client (unspecified) :Publicised Groups",
+};
+
+resource
+publicised_groups_resource
+{
+	"/_matrix/client/r0/publicised_groups",
+	{
+		"(7.5) Lists the public rooms on the server. "
+	}
+};
+
+resource::response
+get__publicised_groups(client &client,
+                       const resource::request &request)
+{
+	return resource::response
+	{
+		client, http::OK
+	};
+}
+
+resource::method
+get_method
+{
+	publicised_groups_resource, "GET", get__publicised_groups
+};
+
+resource::response
+post__publicised_groups(client &client,
+                        const resource::request &request)
+{
+	const json::array &user_ids
+	{
+		request["user_ids"]
+	};
+
+	return resource::response
+	{
+		client, http::OK
+	};
+}
+
+resource::method
+post_method
+{
+	publicised_groups_resource, "POST", post__publicised_groups
+};
