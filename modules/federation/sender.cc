b49cdb84e8fd2f7e93650ce92ea921f81c376c49
Jason Volk <jason@zemos.net>
2018-03-27 01:35:51 -0700

    modules/federation/sender: Synchronize with vm accept under shared lock.

--- modules/federation/sender.cc
+++ modules/federation/sender.cc
@@ -58,7 +58,7 @@ try
 	// In order to synchronize with the vm core, this context has to
 	// maintain this shared_lock at all times. If this is unlocked we
 	// can miss an event being broadcast.
-	std::unique_lock<decltype(m::vm::accept)> lock
+	std::shared_lock<decltype(m::vm::accept)> lock
 	{
 		m::vm::accept
 	};
