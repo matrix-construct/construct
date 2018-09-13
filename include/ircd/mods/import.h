75a05d864c59cf1c4d31c496bfca32a226cd94f4
Jason Volk <jason@zemos.net>
2018-09-13 15:56:11 -0700

    ircd::mods: Minor move import member defs outside of class def.

--- include/ircd/mods/import.h
+++ include/ircd/mods/import.h
@@ -22,25 +22,77 @@ template<class T>
 struct ircd::mods::import
 :sym_ptr
 {
-	template<class... args>
-	auto operator()(args&&... a) const
-	{
-		return sym_ptr::operator()<T>(std::forward<args>(a)...);
-	}
+	template<class... args> auto operator()(args&&... a) const;
+	template<class... args> auto operator()(args&&... a);
 
-	template<class... args>
-	auto operator()(args&&... a)
-	{
-		return sym_ptr::operator()<T>(std::forward<args>(a)...);
-	}
+	const T *operator->() const;
+	const T &operator*() const;
+	operator const T &() const;
 
-	const T *operator->() const                  { return sym_ptr::operator-><T>();                }
-	const T &operator*() const                   { return sym_ptr::operator*<T>();                 }
-	operator const T &() const                   { return sym_ptr::operator*<T>();                 }
-
-	T *operator->()                              { return sym_ptr::operator-><T>();                }
-	T &operator*()                               { return sym_ptr::operator*<T>();                 }
-	operator T &()                               { return sym_ptr::operator*<T>();                 }
+	T *operator->();
+	T &operator*();
+	operator T &();
 
 	using sym_ptr::sym_ptr;
 };
+
+template<class T>
+ircd::mods::import<T>::operator T &()
+{
+	return sym_ptr::operator*<T>();
+}
+
+template<class T>
+T &
+ircd::mods::import<T>::operator*()
+{
+	return sym_ptr::operator*<T>();
+}
+
+template<class T>
+T *
+ircd::mods::import<T>::operator->()
+{
+	return sym_ptr::operator-><T>();
+}
+
+template<class T>
+ircd::mods::import<T>::operator
+const T &()
+const
+{
+	return sym_ptr::operator*<T>();
+}
+
+template<class T>
+const T &
+ircd::mods::import<T>::operator*()
+const
+{
+	return sym_ptr::operator*<T>();
+}
+
+template<class T>
+const T *
+ircd::mods::import<T>::operator->()
+const
+{
+	return sym_ptr::operator-><T>();
+}
+
+template<class T>
+template<class... args>
+auto
+ircd::mods::import<T>::operator()(args&&... a)
+{
+	return sym_ptr::operator()<T>(std::forward<args>(a)...);
+}
+
+template<class T>
+template<class... args>
+auto
+ircd::mods::import<T>::operator()(args&&... a)
+const
+{
+	return sym_ptr::operator()<T>(std::forward<args>(a)...);
+}
