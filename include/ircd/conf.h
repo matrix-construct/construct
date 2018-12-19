566c2ef4ecc1b27ba762cceb91d5d22008c69507
Jason Volk <jason@zemos.net>
2018-12-18 17:00:11 -0800

    ircd::conf: Add int32 conf items.

--- include/ircd/conf.h
+++ include/ircd/conf.h
@@ -31,6 +31,8 @@ namespace ircd::conf
 	template<> struct item<bool>;
 	template<> struct item<uint64_t>;
 	template<> struct item<int64_t>;
+	template<> struct item<uint32_t>;
+	template<> struct item<int32_t>;
 	template<> struct item<float>;
 	template<> struct item<double>;
 	template<> struct item<hours>;
@@ -181,6 +183,20 @@ struct ircd::conf::item<uint64_t>
 template<>
 struct ircd::conf::item<int64_t>
 :lex_castable<int64_t>
+{
+	using lex_castable::lex_castable;
+};
+
+template<>
+struct ircd::conf::item<uint32_t>
+:lex_castable<uint32_t>
+{
+	using lex_castable::lex_castable;
+};
+
+template<>
+struct ircd::conf::item<int32_t>
+:lex_castable<int32_t>
 {
 	using lex_castable::lex_castable;
 };
