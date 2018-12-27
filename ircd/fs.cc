dad9ec8e4424180dda03b7bb7c134d383936f6d5
Jason Volk <jason@zemos.net>
2018-12-27 15:05:21 -0800

    ircd::fs: Increase default io_submit threshold.

--- ircd/fs.cc
+++ ircd/fs.cc
@@ -1046,7 +1046,7 @@ decltype(ircd::fs::aio::max_submit)
 ircd::fs::aio::max_submit
 {
 	{ "name",     "ircd.fs.aio.max_submit"  },
-	{ "default",  16L                       },
+	{ "default",  32L                       },
 	{ "persist",  false                     },
 };
 
