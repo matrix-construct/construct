a13ffd897804aa7d12c0b775a4281169ca553863
Jason Volk <jason@zemos.net>
2019-02-27 18:36:34 -0800

    modules/client/sync/rooms/ephemeral/receipt: Fix competing receipt formats.

--- modules/client/sync/rooms/ephemeral/receipt.cc
+++ modules/client/sync/rooms/ephemeral/receipt.cc
@@ -41,7 +41,12 @@ ircd::m::sync::room_ephemeral_m_receipt_m_read_linear(data &data)
 	if(json::get<"type"_>(*data.event) != "m.receipt")
 		return false;
 
-	_handle_message_receipt(data, *data.event);
+	json::stack::object object
+	{
+		*data.out
+	};
+
+	object.append(*data.event);
 	return true;
 }
 
