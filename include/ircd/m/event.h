04e5e44c98e44a03f2343be8dd1fca21e3994fad
Jason Volk <jason@zemos.net>
2019-05-02 02:24:49 -0700

    ircd::m::event: Use direct reference initialization for constexprs here.

--- include/ircd/m/event.h
+++ include/ircd/m/event.h
@@ -109,10 +109,10 @@ struct ircd::m::event
 	using closure_idx_bool = std::function<bool (const idx &)>;
 	using closure_iov_mutable = std::function<void (json::iov &)>;
 
-	static constexpr const size_t MAX_SIZE = 64_KiB;
-	static constexpr const size_t TYPE_MAX_SIZE = 256;
-	static constexpr const size_t STATE_KEY_MAX_SIZE = 256;
-	static constexpr const size_t ORIGIN_MAX_SIZE = 256;
+	static constexpr const size_t &MAX_SIZE {64_KiB};
+	static constexpr const size_t &TYPE_MAX_SIZE {256};
+	static constexpr const size_t &STATE_KEY_MAX_SIZE {256};
+	static constexpr const size_t &ORIGIN_MAX_SIZE {256};
 	static conf::item<size_t> max_size;
 
 	static void essential(json::iov &event, const json::iov &content, const closure_iov_mutable &);
