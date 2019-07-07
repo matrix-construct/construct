bd09a9aba2331981edc33f9d67cb8df3b48058bb
Jason Volk <jason@zemos.net>
2019-07-07 01:30:20 -0700

    modules/client/rooms/members: Implement c2s r0.5.0 query parameters.

--- modules/client/rooms/members.cc
+++ modules/client/rooms/members.cc
@@ -17,12 +17,50 @@ get__members(client &client,
              const resource::request &request,
              const m::room::id &room_id)
 {
-	const m::room room
+	char membershipbuf[32];
+	const auto &membership
 	{
-		room_id
+		url::decode(membershipbuf, request.query["membership"])
 	};
 
-	if(!exists(room))
+	char not_membershipbuf[32];
+	const auto &not_membership
+	{
+		url::decode(not_membershipbuf, request.query["not_membership"])
+	};
+
+	char atbuf[48];
+	const string_view at
+	{
+		url::decode(atbuf, request.query["at"])
+	};
+
+	const auto event_idx
+	{
+		// at is a /sync since token we gave the client. This is simply
+		// an event_idx sequence integer, except during phased-polylog sync
+		// when this is a negative integer. If this is phased sync, we can
+		// parse this token for the snapshot integer.
+		at && !startswith(at, '-')?
+			lex_cast<m::event::idx>(at):
+		startswith(at, '-')?
+			lex_cast<m::event::idx>(split(at, '_').second):
+			m::event::idx{0}
+	};
+
+	const m::event::id::buf event_id
+	{
+		event_idx && event_idx <= m::vm::sequence::retired?
+			m::event_id(event_idx, std::nothrow):
+			m::event::id::buf{}
+	};
+
+	const m::room room
+	{
+		room_id, event_id
+	};
+
+	if(!event_id && !exists(room))
 		throw m::NOT_FOUND
 		{
 			"Room %s does not exist.",
@@ -61,11 +99,15 @@ get__members(client &client,
 		room
 	};
 
-	members.for_each([&request, &chunk]
+	members.for_each(membership, m::event::closure_bool{[&request, &chunk, &not_membership]
 	(const m::event &event)
 	{
+		if(m::membership(event) == not_membership)
+			return true;
+
 		chunk.append(event);
-	});
+		return true;
+	}});
 
 	return std::move(response);
 }
