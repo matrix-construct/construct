e6a9394a46e044955f4970d86f39c762c2b7dd55
Jason Volk <jason@zemos.net>
2019-08-14 01:41:17 -0700

    ircd::m::init::backfill: Acquire remote head.
    ircd::m::init::backfill: To the fetch unit.

--- modules/m_init_backfill.cc
+++ modules/m_init_backfill.cc
@@ -79,21 +79,38 @@ try
 
 	// Iterate all of the rooms this server is aware of which contain
 	// at least one user from another server which is joined to the room.
-	m::rooms::opts opts;
+	rooms::opts opts;
 	opts.remote_joined_only = true;
-	m::rooms::for_each(opts, []
-	(const m::room::id &room_id)
+
+	size_t count(0);
+	rooms::for_each(opts, [&count]
+	(const room::id &room_id)
 	{
 		handle_room(room_id);
+		++count;
 		return true;
 	});
+
+	log::info
+	{
+		log, "Initial resynchronization of %zu rooms complete.",
+		count,
+	};
 }
 catch(const ctx::interrupted &e)
 {
 	log::derror
 	{
-		log, "Worker interrupted :%s",
-		e.what(),
+		log, "Worker interrupted without completing resynchronization."
+	};
+
+	throw;
+}
+catch(const ctx::terminated &e)
+{
+	log::error
+	{
+		log, "Worker terminated without completing resynchronization."
 	};
 
 	throw;
@@ -116,7 +133,7 @@ try
 		room_id
 	};
 
-	const m::room::origins origins
+	const room::origins origins
 	{
 		room
 	};
@@ -128,6 +145,77 @@ try
 		origins.count(),
 	};
 
+	// When the room isn't public we need to supply a user_id of one of our
+	// users in the room to satisfy matrix protocol requirements upstack.
+	const auto user_id
+	{
+		m::any_user(room, my_host(), "join")
+	};
+
+	size_t respond(0), behind(0), equal(0), ahead(0), exists(0), fetching(0);
+	const auto &[top_event_id, top_depth, top_event_idx]
+	{
+		m::top(std::nothrow, room)
+	};
+
+	feds::opts opts;
+	opts.op = feds::op::head;
+	opts.room_id = room_id;
+	opts.user_id = user_id;
+	opts.closure_errors = false;
+	opts.exclude_myself = true;
+	feds::execute(opts, [&](const auto &result)
+	{
+		const m::event event
+		{
+			result.object.get("event")
+		};
+
+		// The depth comes back as one greater than any existing
+		// depth so we subtract one.
+		const auto &depth
+		{
+			std::max(json::get<"depth"_>(event) - 1L, 0L)
+		};
+
+		ahead += depth > top_depth;
+		equal += depth == top_depth;
+		behind += depth < top_depth;
+		respond++;
+
+		const event::prev prev{event};
+		m::for_each(prev, [&](const event::id &event_id)
+		{
+			if(m::exists(event_id))
+			{
+				++exists;
+				return true;
+			}
+
+			fetching += fetch::start(room_id, event_id);
+			return true;
+		});
+
+		return true;
+	});
+
+	log::info
+	{
+		log, "acquired %s remote head; depth:%ld servers:%zu online:%zu error:%zu"
+		" respond:%zu behind:%zu equal:%zu ahead:%zu fetching:%zu",
+		string_view{room_id},
+		top_depth,
+		origins.count(),
+		origins.count_online(),
+		origins.count_error(),
+		respond,
+		behind,
+		equal,
+		ahead,
+		fetching,
+	};
+
+	assert(ahead + equal + behind == respond);
 }
 catch(const std::exception &e)
 {
