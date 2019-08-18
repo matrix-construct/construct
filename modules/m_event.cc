59a6d4f6b57ca243e8c24942157b47f73011ef74
Jason Volk <jason@zemos.net>
2019-08-17 23:02:07 -0700

    modules: Stub modules in place for m_event/m_room; reorder load list.

---
+++ modules/m_event.cc
@@ -0,0 +1,20 @@
+// Matrix Construct
+//
+// Copyright (C) Matrix Construct Developers, Authors & Contributors
+// Copyright (C) 2016-2019 Jason Volk <jason@zemos.net>
+//
+// Permission to use, copy, modify, and/or distribute this software for any
+// purpose with or without fee is hereby granted, provided that the above
+// copyright notice and this permission notice is present in all copies. The
+// full license for this software is available in the LICENSE file.
+
+// NOTE: !!!
+// Definitions re currently split between libircd and modules until the API
+// and dependency graph has stabilized. Eventually most/all of ircd::m::
+// should migrate out of libircd into modules.
+
+ircd::mapi::header
+IRCD_MODULE
+{
+	"Matrix event library"
+};
