45e5104a8778f4b53bd3a32690bfef528095fa58
Jason Volk <jason@zemos.net>
2019-12-21 14:37:41 -0800

    ircd::leb128: Add RB_GENERIC condition on platform intrinsics.

--- include/ircd/leb128.h
+++ include/ircd/leb128.h
@@ -105,7 +105,7 @@ noexcept
 /// counting the trailing (least significant) zero bits; then add one for
 /// the terminating byte itself. Note doc sez if mask had all zero bits then
 /// the result of clz/ctz is undefined.
-#if defined(__MMX__)
+#if defined(__MMX__) && !defined(RB_GENERIC)
 template<>
 inline size_t
 ircd::uleb128::length(const uint64_t &val)
@@ -122,7 +122,7 @@ noexcept
 }
 #endif
 
-#if defined(__SSE2__)
+#if defined(__SSE2__) && !defined(RB_GENERIC)
 template<>
 inline size_t
 ircd::uleb128::length(const uint128_t &val)
