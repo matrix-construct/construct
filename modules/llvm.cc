fd382d88a006c38f8e1dc585aa6c5ecfb24c6b36
Jason Volk <jason@zemos.net>
2020-02-18 15:48:59 -0800

    modules/llvm: Add info::versions information.

--- modules/llvm.cc
+++ modules/llvm.cc
@@ -8,10 +8,13 @@
 // copyright notice and this permission notice is present in all copies. The
 // full license for this software is available in the LICENSE file.
 
+#include <llvm/Config/llvm-config.h>
 #include <llvm/Object/Wasm.h>
 
 namespace ircd::llvm
 {
+	extern info::versions version_api, version_abi;
+
 	static void init(), fini();
 }
 
@@ -21,6 +24,24 @@ IRCD_MODULE
 	"LLVM Compiler Infrastructure",
 	ircd::llvm::init,
 	ircd::llvm::fini,
+};
+
+decltype(ircd::llvm::version_api)
+ircd::llvm::version_api
+{
+    "llvm", info::versions::API, 0,
+    {
+		LLVM_VERSION_MAJOR,
+		LLVM_VERSION_MINOR,
+		LLVM_VERSION_PATCH,
+    },
+    LLVM_VERSION_STRING,
+};
+
+decltype(ircd::llvm::version_abi)
+ircd::llvm::version_abi
+{
+    "llvm", info::versions::ABI, 0
 };
 
 void
