f941c52dee802d4e1c6be65b0c7113da6b1db971
Jason Volk <jason@zemos.net>
2020-04-03 17:14:35 -0700

    ircd::m: Fix type of membership_positive/membership_negative convenience vectors.

--- include/ircd/m/membership.h
+++ include/ircd/m/membership.h
@@ -40,8 +40,9 @@ namespace ircd::m
 	bool membership(const event::idx &, const vector_view<const string_view> &);
 	bool membership(const room &, const id::user &, const vector_view<const string_view> &);
 
-	extern const vector_view<const string_view> membership_positive; // join, invite
-	extern const vector_view<const string_view> membership_negative; // leave, ban, [non-membership]
+	// Convenience definitions of membership states for atomic queries.
+	extern const std::initializer_list<const string_view> membership_positive; // join, invite
+	extern const std::initializer_list<const string_view> membership_negative; // leave, ban, [non-membership]
 }
 
 inline bool
