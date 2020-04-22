05687190302c0a73408cda7e5c17e8e55a053e95
Jason Volk <jason@zemos.net>
2020-04-21 20:02:27 -0700

    modules/client/rooms/join: Convey any available (non-spec) server_name to m::join().

--- modules/client/rooms/join.cc
+++ modules/client/rooms/join.cc
@@ -17,14 +17,37 @@ post__join(client &client,
            const m::resource::request &request,
            const m::room::id &room_id)
 {
+	static const size_t server_name_maxarg{16U};
+	const auto server_name_count
+	{
+		std::min(request.query.count("server_name"), server_name_maxarg)
+	};
+
+	string_view server_name[server_name_maxarg];
+	const unique_mutable_buffer server_name_buf
+	{
+		rfc3986::DOMAIN_BUFSIZE * server_name_count
+	};
+
+	const auto query_server_names
+	{
+		request.query.array(server_name_buf, "server_name", server_name)
+	};
+
+	//XXX ???
+	const json::string &content_server_names
+	{
+		request["server_name"]
+	};
+
+	const auto &server_names
+	{
+		query_server_names
+	};
+
 	const json::string &third_party_signed
 	{
 		request["third_party_signed"]
-	};
-
-	const json::string &server_name
-	{
-		request["server_name"]
 	};
 
 	const m::room room
@@ -34,7 +57,7 @@ post__join(client &client,
 
 	const auto event_id
 	{
-		m::join(room, request.user_id)
+		m::join(room, request.user_id, server_names)
 	};
 
 	return m::resource::response
