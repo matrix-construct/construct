32d624e3e95bf4d0b11c39e047366a44123725ab
Jason Volk <jason@zemos.net>
2020-04-23 04:01:53 -0700

    ircd::util: Add overload for what() on std::exception value-type.

--- include/ircd/util/what.h
+++ include/ircd/util/what.h
@@ -11,8 +11,15 @@
 #pragma once
 #define HAVE_IRCD_UTIL_WHAT_H
 
-namespace ircd {
-inline namespace util
+namespace ircd { inline namespace util
 {
 	string_view what(const std::exception_ptr eptr = std::current_exception()) noexcept;
+	string_view what(const std::exception &) noexcept;
 }}
+
+inline ircd::string_view
+ircd::util::what(const std::exception &e)
+noexcept
+{
+	return e.what();
+}
