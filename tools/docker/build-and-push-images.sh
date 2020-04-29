cbe7d13c6f741cf67b58b02f66a10230effc2d2a
Jason Volk <jason@zemos.net>
2020-04-29 02:07:16 -0700

    docker/appveyor: RocksDB 5.17.2; Boost 1.71; remove PPAs for 20.04 vocal fossil.

--- tools/docker/build-and-push-images.sh
+++ tools/docker/build-and-push-images.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 BASEDIR=$(dirname "$0")
 
-docker pull ubuntu:18.04
-docker build -t jevolk/construct:ubuntu-18.04 $BASEDIR/ubuntu/base
-docker build -t jevolk/construct:ubuntu-18.04-clang-9 $BASEDIR/ubuntu/clang-9
-docker build -t jevolk/construct:ubuntu-18.04-gcc-8 $BASEDIR/ubuntu/gcc-8
-docker build -t jevolk/construct:ubuntu-18.04-gcc-9 $BASEDIR/ubuntu/gcc-9
+docker pull ubuntu:20.04
+docker build -t jevolk/construct:ubuntu-20.04 $BASEDIR/ubuntu/base
+docker build -t jevolk/construct:ubuntu-20.04-clang-9 $BASEDIR/ubuntu/clang-9
+docker build -t jevolk/construct:ubuntu-20.04-gcc-8 $BASEDIR/ubuntu/gcc-8
+docker build -t jevolk/construct:ubuntu-20.04-gcc-9 $BASEDIR/ubuntu/gcc-9
 
-docker push jevolk/construct:ubuntu-18.04-clang-9
-docker push jevolk/construct:ubuntu-18.04-gcc-8
-docker push jevolk/construct:ubuntu-18.04-gcc-9
+docker push jevolk/construct:ubuntu-20.04-clang-9
+docker push jevolk/construct:ubuntu-20.04-gcc-8
+docker push jevolk/construct:ubuntu-20.04-gcc-9
