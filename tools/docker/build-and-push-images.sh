5e9701b6c9f9f71a73c78c7467d6a30fe4d9503a
Jason Volk <jason@zemos.net>
2020-04-29 11:09:45 -0700

    tools/docker: Trim docker pull from build and push script.

--- tools/docker/build-and-push-images.sh
+++ tools/docker/build-and-push-images.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 BASEDIR=$(dirname "$0")
 
-docker pull ubuntu:20.04
 docker build -t jevolk/construct:ubuntu-20.04 $BASEDIR/ubuntu/base
 docker build -t jevolk/construct:ubuntu-20.04-clang-9 $BASEDIR/ubuntu/clang-9
 docker build -t jevolk/construct:ubuntu-20.04-gcc-8 $BASEDIR/ubuntu/gcc-8
