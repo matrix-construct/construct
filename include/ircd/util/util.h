52831893dac261ec0c3a18d4e776972b0498a41c
Jason Volk <jason@zemos.net>
2020-05-07 21:31:02 -0700

    ircd::util: Add next_powerof2(u64) constexpr.

--- include/ircd/util/util.h
+++ include/ircd/util/util.h
@@ -258,6 +258,19 @@ is_powerof2(const long long v)
 	return v && !(v & (v - 1LL));
 }
 
+constexpr uint64_t
+next_powerof2(uint64_t v)
+{
+	--v;
+	v |= v >> 1;
+	v |= v >> 2;
+	v |= v >> 4;
+	v |= v >> 8;
+	v |= v >> 16;
+	v |= v >> 32;
+	return ++v;
+}
+
 template<class T>
 T
 minmax(T ret,
