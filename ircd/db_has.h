1e788c09f30b8bc6d75fd47c319b7788f1140e71
Jason Volk <jason@zemos.net>
2020-06-07 05:48:07 -0700

    ircd::db: Sort feature defines.

--- ircd/db_has.h
+++ ircd/db_has.h
@@ -8,10 +8,21 @@
 // copyright notice and this permission notice is present in all copies. The
 // full license for this software is available in the LICENSE file.
 
+#if ROCKSDB_MAJOR > 5 \
+|| (ROCKSDB_MAJOR == 5 && ROCKSDB_MINOR >= 18)
+	#define IRCD_DB_HAS_ALLOCATOR
+#endif
+
 #if ROCKSDB_MAJOR > 6 \
 || (ROCKSDB_MAJOR == 6 && ROCKSDB_MINOR > 1) \
 || (ROCKSDB_MAJOR == 6 && ROCKSDB_MINOR == 1 && ROCKSDB_PATCH >= 1)
 	#define IRCD_DB_HAS_ENV_PRIO_USER
+#endif
+
+#if ROCKSDB_MAJOR > 6 \
+|| (ROCKSDB_MAJOR == 6 && ROCKSDB_MINOR > 2) \
+|| (ROCKSDB_MAJOR == 6 && ROCKSDB_MINOR == 2 && ROCKSDB_PATCH >= 2)
+	#define IRCD_DB_HAS_PERIODIC_COMPACTIONS
 #endif
 
 #if ROCKSDB_MAJOR > 6 \
@@ -21,24 +32,13 @@
 #endif
 
 #if ROCKSDB_MAJOR > 6 \
-|| (ROCKSDB_MAJOR == 6 && ROCKSDB_MINOR > 7) \
-|| (ROCKSDB_MAJOR == 6 && ROCKSDB_MINOR == 7 && ROCKSDB_PATCH >= 3)
-	#define IRCD_DB_HAS_ENV_FILESYSTEM
-#endif
-
-#if ROCKSDB_MAJOR > 6 \
 || (ROCKSDB_MAJOR == 6 && ROCKSDB_MINOR > 4) \
 || (ROCKSDB_MAJOR == 6 && ROCKSDB_MINOR == 4 && ROCKSDB_PATCH >= 6)
 	#define IRCD_DB_HAS_CACHE_GETCHARGE
 #endif
 
-#if ROCKSDB_MAJOR > 5 \
-|| (ROCKSDB_MAJOR == 5 && ROCKSDB_MINOR >= 18)
-	#define IRCD_DB_HAS_ALLOCATOR
-#endif
-
 #if ROCKSDB_MAJOR > 6 \
-|| (ROCKSDB_MAJOR == 6 && ROCKSDB_MINOR > 2) \
-|| (ROCKSDB_MAJOR == 6 && ROCKSDB_MINOR == 2 && ROCKSDB_PATCH >= 2)
-	#define IRCD_DB_HAS_PERIODIC_COMPACTIONS
+|| (ROCKSDB_MAJOR == 6 && ROCKSDB_MINOR > 7) \
+|| (ROCKSDB_MAJOR == 6 && ROCKSDB_MINOR == 7 && ROCKSDB_PATCH >= 3)
+	#define IRCD_DB_HAS_ENV_FILESYSTEM
 #endif
