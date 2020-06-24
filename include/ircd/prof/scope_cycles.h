3d1042332871d9376314ad9af85be868a54f4fa2
Jason Volk <jason@zemos.net>
2020-06-24 08:50:31 -0700

    ircd::prof: Add template options to constexpr-condition fence instructions.

--- include/ircd/prof/scope_cycles.h
+++ include/ircd/prof/scope_cycles.h
@@ -13,12 +13,21 @@
 
 namespace ircd::prof
 {
+	template<bool fenced = false>
 	struct scope_cycles;
 }
 
 /// Count the reference cycles for a scope using the lifetime of this object.
 /// The result is added to the value in `result`; note that result must be
 /// initialized.
+///
+/// The 'fenced' option in the template enables fence instructions as
+/// prescribed by amd64 manuals. The fences provide a more accurate result for
+/// the section being counted at the cost of additional general overhead. This
+/// is disabled by default so that permanent instances of this device are not
+/// interfering and don't require any templating.
+///
+template<bool fenced>
 struct ircd::prof::scope_cycles
 {
 	uint64_t &result;
@@ -29,46 +38,60 @@ struct ircd::prof::scope_cycles
 	~scope_cycles() noexcept;
 };
 
+template<bool fenced>
 #ifdef __clang__
 inline //TODO: ???
 #else
 extern inline
 __attribute__((flatten, always_inline, gnu_inline, artificial))
 #endif
-ircd::prof::scope_cycles::scope_cycles(uint64_t &result)
+ircd::prof::scope_cycles<fenced>::scope_cycles(uint64_t &result)
 noexcept
 :result{result}
 {
-	#if defined(__x86_64__) || defined(__i386__)
-	asm volatile ("mfence");
-	asm volatile ("lfence");
-	#endif
+	if constexpr(fenced)
+	{
+		#if defined(__x86_64__) || defined(__i386__)
+		asm volatile ("mfence");
+		asm volatile ("lfence");
+		#endif
+	}
 
 	started = cycles();
 
-	#if defined(__x86_64__) || defined(__i386__)
-	asm volatile ("lfence");
-	#endif
+	if constexpr(fenced)
+	{
+		#if defined(__x86_64__) || defined(__i386__)
+		asm volatile ("lfence");
+		#endif
+	}
 }
 
+template<bool fenced>
 #ifdef __clang__
 inline //TODO: ???
 #else
 extern inline
 __attribute__((flatten, always_inline, gnu_inline, artificial))
 #endif
-ircd::prof::scope_cycles::~scope_cycles()
+ircd::prof::scope_cycles<fenced>::~scope_cycles()
 noexcept
 {
-	#if defined(__x86_64__) || defined(__i386__)
-	asm volatile ("mfence");
-	asm volatile ("lfence");
-	#endif
+	if constexpr(fenced)
+	{
+		#if defined(__x86_64__) || defined(__i386__)
+		asm volatile ("mfence");
+		asm volatile ("lfence");
+		#endif
+	}
 
 	const uint64_t stopped(cycles());
 	result += stopped - started;
 
-	#if defined(__x86_64__) || defined(__i386__)
-	asm volatile ("lfence");
-	#endif
+	if constexpr(fenced)
+	{
+		#if defined(__x86_64__) || defined(__i386__)
+		asm volatile ("lfence");
+		#endif
+	}
 }
