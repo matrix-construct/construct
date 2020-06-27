d26fed6a204b4532dddcb79c2cf0679c865afe65
Jason Volk <jason@zemos.net>
2020-06-27 15:21:37 -0700

    ircd::simd: Add basic traits constexprs.

--- include/ircd/simd/traits.h
+++ include/ircd/simd/traits.h
@@ -13,6 +13,12 @@
 
 namespace ircd::simd
 {
+	template<class T>
+	constexpr size_t sizeof_lane(T = {});
+
+	template<class T>
+	constexpr size_t lanes(T = {});
+
 	// lane number convenience constants
 	extern const u8x64    u8x64_lane_id;
 	extern const u8x32    u8x32_lane_id;
@@ -30,3 +36,103 @@ namespace ircd::simd
 	extern const u256x1  u256x1_lane_id;
 	extern const u512x1  u512x1_lane_id;
 }
+
+/// Get number of lanes for vector type (the number after the x in the
+/// type name).
+template<class T>
+constexpr size_t
+ircd::simd::lanes(T)
+{
+	constexpr size_t ret
+	{
+		sizeof(T) / sizeof_lane<T>()
+	};
+
+	static_assert
+	(
+		sizeof(T) <= 64 && ret <= 64 && ret > 0
+	);
+
+	return ret;
+}
+
+/// Get the size of each lane; i.e the size of one integral element.
+template<class T>
+constexpr size_t
+ircd::simd::sizeof_lane(T)
+{
+	constexpr size_t ret
+	{
+		sizeof(T{}[0])
+	};
+
+	static_assert
+	(
+		ret >= 1 && ret <= 64
+	);
+
+	return ret;
+}
+
+template<>
+constexpr size_t
+ircd::simd::sizeof_lane(u128x1)
+{
+	return 16;
+};
+
+template<>
+constexpr size_t
+ircd::simd::sizeof_lane(f128x1)
+{
+	return 16;
+};
+
+template<>
+constexpr size_t
+ircd::simd::sizeof_lane(d128x1)
+{
+	return 16;
+};
+
+template<>
+constexpr size_t
+ircd::simd::sizeof_lane(u256x1)
+{
+	return 32;
+};
+
+template<>
+constexpr size_t
+ircd::simd::sizeof_lane(f256x1)
+{
+	return 32;
+};
+
+template<>
+constexpr size_t
+ircd::simd::sizeof_lane(d256x1)
+{
+	return 32;
+};
+
+template<>
+constexpr size_t
+ircd::simd::sizeof_lane(u512x1)
+{
+	return 64;
+};
+
+template<>
+constexpr size_t
+ircd::simd::sizeof_lane(f512x1)
+{
+	return 64;
+};
+
+template<>
+constexpr size_t
+ircd::simd::sizeof_lane(d512x1)
+{
+	return 64;
+};
