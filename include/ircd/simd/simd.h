aec21af0e409e225448f0c636ea70bf9c5144387
Jason Volk <jason@zemos.net>
2020-06-29 20:15:11 -0700

    ircd::simd: Add population mask convenience template.

--- include/ircd/simd/simd.h
+++ include/ircd/simd/simd.h
@@ -18,6 +18,9 @@
 
 namespace ircd::simd
 {
+	template<class T> T popmask(const T) noexcept;
+	template<class T> size_t popcount(const T) noexcept;
+
 	// xmmx shifter workarounds
 	template<int bits, class T> T shl(const T &a) noexcept;
 	template<int bits, class T> T shr(const T &a) noexcept;
@@ -32,6 +35,32 @@ namespace ircd
 	using simd::shl;
 	using simd::shr;
 	using simd::lane_cast;
+}
+
+/// Convenience template. Vector compare instructions yield 0xff on equal;
+/// sometimes one might need an actual value of 1 for accumulators or maybe
+/// some bool-type reason...
+template<class T>
+inline size_t
+ircd::simd::popcount(const T a)
+noexcept
+{
+	size_t i(0), ret(0);
+	while(i < lanes(a))
+		ret += __builtin_popcountll(a[i++]);
+
+	return ret;
+}
+
+/// Convenience template. Vector compare instructions yield 0xff on equal;
+/// sometimes one might need an actual value of 1 for accumulators or maybe
+/// some bool-type reason...
+template<class T>
+inline T
+ircd::simd::popmask(const T a)
+noexcept
+{
+	return a & 1;
 }
 
 #ifdef HAVE_X86INTRIN_H
