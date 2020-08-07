52bc00673e278ff80bf0f295c0e78e4dbeb06b1f
Jason Volk <jason@zemos.net>
2020-08-07 01:11:16 -0700

    ircd::simd: Redress unaligned typedefs for proper instruction generation.

--- include/ircd/simd/type.h
+++ include/ircd/simd/type.h
@@ -11,6 +11,26 @@
 #pragma once
 #define HAVE_IRCD_SIMD_TYPE_H
 
+/// Unaligned type wrapper macro
+#define IRCD_SIMD_TYPEDEF_UNALIGNED(TYPE, NAME)  \
+struct [[gnu::visibility("internal")]]           \
+NAME                                             \
+{                                                \
+    TYPE val;                                    \
+                                                 \
+    operator TYPE () const { return val; }       \
+    operator TYPE &()      { return val; }       \
+                                                 \
+    template<class T> NAME &operator=(T&& t)     \
+    {                                            \
+        val = std::forward<T>(t);                \
+        return *this;                            \
+    }                                            \
+}                                                \
+__attribute__((packed))                          \
+__attribute__((aligned(1)))
+
+
 //
 // scalar
 //
@@ -52,11 +72,12 @@ namespace ircd
 	typedef __v4du        u64x4;     //  [____0____|____1____|____2____|____3____|
 	typedef __v2du        u64x2;     //  [____0____|____1____|
 	typedef __m128i       u128x1;    //  [________0________|
-	typedef __m128i_u     u128x1_u;  // unaligned
 	typedef __m256i       u256x1;    //  [________________0________________|
-	typedef __m256i_u     u256x1_u;  // unaligned
 	typedef __m512i       u512x1;    //  [_______________________________0________________________________|
-	typedef __m512i_u     u512x1_u;  // unaligned
+
+	IRCD_SIMD_TYPEDEF_UNALIGNED(__m128i, u128x1_u);
+	IRCD_SIMD_TYPEDEF_UNALIGNED(__m256i, u256x1_u);
+	IRCD_SIMD_TYPEDEF_UNALIGNED(__m512i, u512x1_u);
 }
 #endif
 
@@ -80,11 +101,12 @@ namespace ircd
 	typedef __v4di        i64x4;     //  [____0____|____1____|____2____|____3____|
 	typedef __v2di        i64x2;     //  [____0____|____1____|
 	typedef __m128i       i128x1;    //  [________0________]
-	typedef __m128i_u     i128x1_u;  // unaligned
 	typedef __m256i       i256x1;    //  [________________0________________|
-	typedef __m256i_u     i256x1_u;  // unaligned
 	typedef __m512i       i512x1;    //  [_______________________________0________________________________|
-	typedef __m512i_u     i512x1_u;  // unaligned
+
+	IRCD_SIMD_TYPEDEF_UNALIGNED(__m128i, i128x1_u);
+	IRCD_SIMD_TYPEDEF_UNALIGNED(__m256i, i256x1_u);
+	IRCD_SIMD_TYPEDEF_UNALIGNED(__m512i, i512x1_u);
 }
 #endif
 
@@ -100,11 +122,12 @@ namespace ircd
 	typedef __v8sf        f32x8;     //  [__0__|__1__|__2__|__3__|__4__|__5__|__6__|__7__|
 	typedef __v4sf        f32x4;     //  [__0__|__1__|__2__|__3__|
 	typedef __m128        f128x1;    //  [____|____0____|____|
-	typedef __m128_u      f128x1_u;  // unaligned
 	typedef __m256        f256x1;    //  [________________0________________|
-	typedef __m256_u      f256x1_u;  // unaligned
 	typedef __m512        f512x1;    //  [_______________________________0________________________________|
-	typedef __m512_u      f512x1_u;  // unaligned
+
+	IRCD_SIMD_TYPEDEF_UNALIGNED(__m128, f128x1_u);
+	IRCD_SIMD_TYPEDEF_UNALIGNED(__m256, f256x1_u);
+	IRCD_SIMD_TYPEDEF_UNALIGNED(__m512, f512x1_u);
 }
 #endif
 
@@ -119,11 +142,12 @@ namespace ircd
 	typedef __v4df        f64x4;     //  [____0____|____1____|____2____|____3____|
 	typedef __v2df        f64x2;     //  [____0____|____1____|
 	typedef __m128d       d128x1;    //  [________0________]
-	typedef __m128d_u     d128x1_u;  // unaligned
 	typedef __m256d       d256x1;    //  [________________0________________|
-	typedef __m256d_u     d256x1_u;  // unaligned
 	typedef __m512d       d512x1;    // [_______________________________0________________________________|
-	typedef __m512d_u     d512x1_u;  // unaligned
+
+	IRCD_SIMD_TYPEDEF_UNALIGNED(__m128d, d128x1_u);
+	IRCD_SIMD_TYPEDEF_UNALIGNED(__m256d, d256x1_u);
+	IRCD_SIMD_TYPEDEF_UNALIGNED(__m512d, d512x1_u);
 }
 #endif
 
