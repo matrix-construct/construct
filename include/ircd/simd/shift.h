97deec5c68317a287d01bc3c88f53681de673020
Jason Volk <jason@zemos.net>
2020-08-06 23:34:37 -0700

    ircd::simd: Additional vector sizes and scaling template for shift suite.

--- include/ircd/simd/shift.h
+++ include/ircd/simd/shift.h
@@ -11,7 +11,7 @@
 #pragma once
 #define HAVE_IRCD_SIMD_SHIFT_H
 
-// xmmx shifter workarounds
+// xmmx shifter interface
 namespace ircd::simd
 {
 	template<int b,
@@ -26,6 +26,11 @@ namespace ircd::simd
 
 	template<int b,
              class T>
+	typename std::enable_if<sizeof(T) == 64, T>::type
+	shl(const T a) noexcept;
+
+	template<int b,
+             class T>
 	typename std::enable_if<sizeof(T) == 16, T>::type
 	shr(const T a) noexcept;
 
@@ -33,6 +38,25 @@ namespace ircd::simd
              class T>
 	typename std::enable_if<sizeof(T) == 32, T>::type
 	shr(const T a) noexcept;
+
+	template<int b,
+             class T>
+	typename std::enable_if<sizeof(T) == 64, T>::type
+	shr(const T a) noexcept;
+}
+
+// xmmx shifter fallback template (internal)
+namespace ircd::simd
+{
+	template<int b,
+             class V,
+             class T>
+	T _shl(const T) noexcept;
+
+	template<int b,
+	         class V,
+             class T>
+	T _shr(const T) noexcept;
 }
 
 #ifdef HAVE_X86INTRIN_H
@@ -84,6 +108,16 @@ noexcept
 
 	return T(_mm256_srli_si256(u256x1(a), b / 8));
 }
+#else
+template<int b,
+         class T>
+[[using gnu: always_inline, gnu_inline, artificial]]
+extern inline typename std::enable_if<sizeof(T) == 32, T>::type
+ircd::simd::shr(const T a)
+noexcept
+{
+	return _shr<b, u8x32>(a);
+}
 #endif
 
 #if defined(HAVE_X86INTRIN_H) && defined(__AVX2__)
@@ -101,4 +135,134 @@ noexcept
 
 	return T(_mm256_slli_si256(u256x1(a), b / 8));
 }
+#else
+template<int b,
+         class T>
+[[using gnu: always_inline, gnu_inline, artificial]]
+extern inline typename std::enable_if<sizeof(T) == 32, T>::type
+ircd::simd::shl(const T a)
+noexcept
+{
+	return _shl<b, u8x32>(a);
+}
 #endif
+
+#if defined(HAVE_X86INTRIN_H) && defined(__AVX512F__)
+template<int b,
+         class T>
+[[using gnu: always_inline, gnu_inline, artificial]]
+extern inline typename std::enable_if<sizeof(T) == 64, T>::type
+ircd::simd::shr(const T a)
+noexcept
+{
+	static_assert
+	(
+		b % 8 == 0, "ymmx register only shifts right at bytewise resolution."
+	);
+
+	return T(_mm512_srli_si512(u512x1(a), b / 8));
+}
+#else
+template<int b,
+         class T>
+[[using gnu: always_inline, gnu_inline, artificial]]
+extern inline typename std::enable_if<sizeof(T) == 64, T>::type
+ircd::simd::shr(const T a)
+noexcept
+{
+	return _shr<b, u8x64>(a);
+}
+#endif
+
+#if defined(HAVE_X86INTRIN_H) && defined(__AVX512F__)
+template<int b,
+         class T>
+[[using gnu: always_inline, gnu_inline, artificial]]
+extern inline typename std::enable_if<sizeof(T) == 64, T>::type
+ircd::simd::shl(const T a)
+noexcept
+{
+	static_assert
+	(
+		b % 8 == 0, "ymmx register only shifts left at bytewise resolution."
+	);
+
+	return T(_mm512_slli_si512(u512x1(a), b / 8));
+}
+#else
+template<int b,
+         class T>
+[[using gnu: always_inline, gnu_inline, artificial]]
+extern inline typename std::enable_if<sizeof(T) == 64, T>::type
+ircd::simd::shl(const T a)
+noexcept
+{
+	return _shl<b, u8x64>(a);
+}
+#endif
+
+template<int b,
+         class V,
+         class T>
+[[using gnu: always_inline, gnu_inline, artificial]]
+extern inline T
+ircd::simd::_shr(const T a)
+noexcept
+{
+	static_assert
+	(
+		sizeof(T) == sizeof(V)
+	);
+
+	static_assert
+	(
+		b % 8 == 0, "[emulated] xmmx register only shifts right at bytewise resolution."
+	);
+
+	constexpr int B
+	{
+		b / 8
+	};
+
+	V ret;
+	for(size_t i(0); i < sizeof(V) - B; ++i)
+		ret[i] = a[i + B];
+
+	for(size_t i(sizeof(V) - B); i < sizeof(V); ++i)
+		ret[i] = 0;
+
+	return T(ret);
+}
+
+template<int b,
+         class V,
+         class T>
+[[using gnu: always_inline, gnu_inline, artificial]]
+extern inline T
+ircd::simd::_shl(const T a)
+noexcept
+{
+	static_assert
+	(
+		sizeof(T) == sizeof(V)
+	);
+
+	static_assert
+	(
+		b % 8 == 0, "[emulated] xmmx register only shifts left at bytewise resolution."
+	);
+
+	constexpr int B
+	{
+		b / 8
+	};
+
+	V ret;
+	for(size_t i(0); i < B; ++i)
+		ret[i] = 0;
+
+	for(size_t i(B); i < sizeof(V); ++i)
+		ret[i] = a[i - B];
+
+	return T(ret);
+}
