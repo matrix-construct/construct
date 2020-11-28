266e617e36a1a7c974bd0e01941683328e9ce8a1
Jason Volk <jason@zemos.net>
2020-11-27 16:57:31 -0800

    ircd::util: Implement the closure template concept.

--- include/ircd/util/closure.h
+++ include/ircd/util/closure.h
@@ -14,17 +14,70 @@
 namespace ircd {
 inline namespace util
 {
-	template<class function> struct closure;
+	template<template<class, class...>
+	         class F,
+	         class R,
+	         class... A>
+	struct closure;
+
+	template<template<class, class...>
+	         class F,
+	         class... A>
+	struct closure<F, bool, A...>;
+
+	/// Template for creating callback-based iterations allowing closures of
+	/// either `void (...)` or `bool (...)` to be passed to the same overload.
+	///
+	/// This reduces/deduplicates interfaces which offer an overload for each
+	/// and thus two library symbols, usually just calling each other...
+	///
+	template<template<class, class...>
+	         class F,
+	         class... A>
+	using closure_bool = closure<F, bool, A...>;
 }}
 
-///TODO: This is a WIP that is meant to replace the pattern of having two
-///TODO: for_each() overloads in every interface where one takes a closure
-///TODO: which returns a bool, and the other takes a void closure. In the
-///TODO: majority case, the void overload simply calls the bool overload and
-// TODO: returns true. In the minority case, the overloads are optimized. This
-///TODO: device should be useful in the majority case.
-template<class function>
+template<template<class, class...>
+         class F,
+         class R,
+         class... A>
 struct ircd::util::closure
+:F<R (A...)>
 {
+	using proto_type = R (A...);
+	using func_type = F<proto_type>;
 
+	using func_type::func_type;
+	using func_type::operator=;
 };
+
+template<template<class, class...>
+         class F,
+         class... A>
+struct ircd::util::closure<F, bool, A...>
+:F<bool (A...)>
+{
+	using proto_type = bool (A...);
+	using func_type = F<proto_type>;
+
+	template<class lambda>
+	closure(lambda &&o) noexcept;
+};
+
+template<template<class, class...>
+         class F,
+         class... A>
+template<class lambda>
+[[gnu::always_inline]]
+inline
+ircd::util::closure<F, bool, A...>::closure(lambda &&o)
+noexcept
+:F<bool (A...)>
+{
+	[o(std::move(o))](A&&... a)
+	{
+		o(std::forward<A>(a)...);
+		return true;
+	}
+}
+{}
