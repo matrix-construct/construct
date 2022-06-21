d5302087bc0e3eb04ae0ba7c71203248a4f5924d
Jason Volk <jason@zemos.net>
2022-06-21 09:29:39 -0700

    docker: Upgrade images to 22.04; update appveyor build matrix.

--- docker/build-and-push-images.sh
+++ docker/build-and-push-images.sh
@@ -1,15 +1,50 @@
 #!/bin/sh
 BASEDIR=$(dirname "$0")
+ACCT=jevolk
+REPO=construct
+#ARGS="--build-arg skiprocks=1"
 
-docker build -t jevolk/construct:ubuntu-20.04 $BASEDIR/ubuntu/base
-docker build -t jevolk/construct:ubuntu-20.04-clang-9 $BASEDIR/ubuntu/clang-9
-docker build -t jevolk/construct:ubuntu-20.04-clang-10 $BASEDIR/ubuntu/clang-10
-docker build -t jevolk/construct:ubuntu-20.04-gcc-8 $BASEDIR/ubuntu/gcc-8
-docker build -t jevolk/construct:ubuntu-20.04-gcc-9 $BASEDIR/ubuntu/gcc-9
-docker build -t jevolk/construct:ubuntu-20.04-gcc-10 $BASEDIR/ubuntu/gcc-10
+#docker build -t $ACCT/$REPO:ubuntu-20.04 $BASEDIR/ubuntu/20.04/base
+#docker build -t $ACCT/$REPO:ubuntu-20.04-gcc-8 $BASEDIR/ubuntu/20.04/gcc-8
+#docker build -t $ACCT/$REPO:ubuntu-20.04-gcc-9 $BASEDIR/ubuntu/20.04/gcc-9
+#docker build -t $ACCT/$REPO:ubuntu-20.04-gcc-10 $BASEDIR/ubuntu/20.04/gcc-10
+#docker build -t $ACCT/$REPO:ubuntu-20.04-clang-9 $BASEDIR/ubuntu/20.04/clang-9
+#docker build -t $ACCT/$REPO:ubuntu-20.04-clang-10 $BASEDIR/ubuntu/20.04/clang-10
+#docker build -t $ACCT/$REPO:ubuntu-20.04-clang-11 $BASEDIR/ubuntu/20.04/clang-11
+#docker build -t $ACCT/$REPO:ubuntu-20.04-clang-12 $BASEDIR/ubuntu/20.04/clang-12
 
-docker push jevolk/construct:ubuntu-20.04-clang-9
-docker push jevolk/construct:ubuntu-20.04-clang-10
-docker push jevolk/construct:ubuntu-20.04-gcc-8
-docker push jevolk/construct:ubuntu-20.04-gcc-9
-docker push jevolk/construct:ubuntu-20.04-gcc-10
+#docker push $ACCT/$REPO:ubuntu-20.04-gcc-8
+#docker push $ACCT/$REPO:ubuntu-20.04-gcc-9
+#docker push $ACCT/$REPO:ubuntu-20.04-gcc-10
+#docker push $ACCT/$REPO:ubuntu-20.04-clang-9
+#docker push $ACCT/$REPO:ubuntu-20.04-clang-10
+#docker push $ACCT/$REPO:ubuntu-20.04-clang-11
+#docker push $ACCT/$REPO:ubuntu-20.04-clang-12
+
+docker build $ARGS -t $ACCT/$REPO:ubuntu-22.04-base $BASEDIR/ubuntu/22.04/base
+docker build $ARGS -t $ACCT/$REPO:ubuntu-22.04-full $BASEDIR/ubuntu/22.04/full
+#docker build $ARGS -t $ACCT/$REPO:ubuntu-22.04-base-gcc-8 $BASEDIR/ubuntu/22.04/base-gcc-8
+docker build $ARGS -t $ACCT/$REPO:ubuntu-22.04-base-gcc-9 $BASEDIR/ubuntu/22.04/base-gcc-9
+#docker build $ARGS -t $ACCT/$REPO:ubuntu-22.04-base-gcc-10 $BASEDIR/ubuntu/22.04/base-gcc-10
+#docker build $ARGS -t $ACCT/$REPO:ubuntu-22.04-base-gcc-11 $BASEDIR/ubuntu/22.04/base-gcc-11
+docker build $ARGS -t $ACCT/$REPO:ubuntu-22.04-base-gcc-12 $BASEDIR/ubuntu/22.04/base-gcc-12
+#docker build $ARGS -t $ACCT/$REPO:ubuntu-22.04-base-clang-10 $BASEDIR/ubuntu/22.04/base-clang-10
+#docker build $ARGS -t $ACCT/$REPO:ubuntu-22.04-base-clang-11 $BASEDIR/ubuntu/22.04/base-clang-11
+#docker build $ARGS -t $ACCT/$REPO:ubuntu-22.04-base-clang-12 $BASEDIR/ubuntu/22.04/base-clang-12
+#docker build $ARGS -t $ACCT/$REPO:ubuntu-22.04-base-clang-13 $BASEDIR/ubuntu/22.04/base-clang-13
+#docker build $ARGS -t $ACCT/$REPO:ubuntu-22.04-base-clang-14 $BASEDIR/ubuntu/22.04/base-clang-14
+docker build $ARGS -t $ACCT/$REPO:ubuntu-22.04-full-clang-14 $BASEDIR/ubuntu/22.04/full-clang-14
+
+docker push $ACCT/$REPO:ubuntu-22.04-base
+docker push $ACCT/$REPO:ubuntu-22.04-full
+#docker push $ACCT/$REPO:ubuntu-22.04-base-gcc-8
+docker push $ACCT/$REPO:ubuntu-22.04-base-gcc-9
+#docker push $ACCT/$REPO:ubuntu-22.04-base-gcc-10
+#docker push $ACCT/$REPO:ubuntu-22.04-base-gcc-11
+docker push $ACCT/$REPO:ubuntu-22.04-base-gcc-12
+#docker push $ACCT/$REPO:ubuntu-22.04-base-clang-10
+#docker push $ACCT/$REPO:ubuntu-22.04-base-clang-11
+#docker push $ACCT/$REPO:ubuntu-22.04-base-clang-12
+#docker push $ACCT/$REPO:ubuntu-22.04-base-clang-13
+#docker push $ACCT/$REPO:ubuntu-22.04-base-clang-14
+docker push $ACCT/$REPO:ubuntu-22.04-full-clang-14
